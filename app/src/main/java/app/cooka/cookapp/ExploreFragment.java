14471fbb45d91e28544b4e9248397541c7e8f50f
Karl Möller <karl.moeller@stud.hs-hannover.de>
2018-12-07 10:59:05 +0100

    added onMenuItemSelectedListener method to explore fragment toolbar; added linear layout to explore fragment

--- app/src/main/java/app/cooka/cookapp/ExploreFragment.java
+++ app/src/main/java/app/cooka/cookapp/ExploreFragment.java
@@ -8,8 +8,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class ExploreFragment extends Fragment {
 
@@ -36,6 +38,14 @@ public class ExploreFragment extends Fragment {
         //Get the toolbar from the inflated layout and assign the menu
         toolbar = fragmentView.findViewById(R.id.explore_toolbar);
         toolbar.inflateMenu(R.menu.explore_toolbar);
+
+        //Setup the MenuItem click listener to handle toolbar menu actions
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return onToolbarMenuItemClicked(menuItem);
+            }
+        });
 
         //Return the inflated view
         return fragmentView;
@@ -66,5 +76,20 @@ public class ExploreFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    //This method is called when a menu item from the toolbar is selected
+    private boolean onToolbarMenuItemClicked(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_explore_search:
+                //Search action
+                Toast.makeText(this.getActivity(), R.string.action_search, Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.action_settings:
+                //Settings action
+                Toast.makeText(this.getActivity(), R.string.action_settings, Toast.LENGTH_LONG).show();
+                return true;
+        }
+        return false;
     }
 }
--- app/src/main/java/app/cooka/cookapp/MainActivity.java
+++ app/src/main/java/app/cooka/cookapp/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity implements OnFragmentInterac
         cookFragment = new CookFragment();
 
         //Load explore fragment
-        //loadFragment(exploreFragment);
+        loadFragment(exploreFragment);
 
         //Get reference to bottom navigation bar
         navigationView = (BottomNavigationView) findViewById(R.id.navigation);
