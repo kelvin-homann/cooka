e8b63d4517b530624cfb1584b93e0d418fdc5651
Karl Möller <karl.moeller@stud.hs-hannover.de>
2018-12-10 16:08:32 +0100

    added toolbar to cookbook, plan and cook fragment

--- app/src/main/java/app/cooka/cookapp/CookbookFragment.java
+++ app/src/main/java/app/cooka/cookapp/CookbookFragment.java
@@ -4,24 +4,42 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 
 public class CookbookFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
+    private Toolbar toolbar;
 
     public CookbookFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cookbook, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //Inflate the fragment view
+        View fragmentView = inflater.inflate(R.layout.fragment_cookbook, container, false);
+
+        //Get the toolbar from the inflated layout and assign the menu
+        toolbar = fragmentView.findViewById(R.id.cookbook_toolbar);
+        toolbar.inflateMenu(R.menu.cookbook_toolbar);
+
+        //Setup the MenuItem click listener to handle toolbar menu actions
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return onToolbarMenuItemClicked(menuItem);
+            }
+        });
+
+        //Return the inflated view
+        return fragmentView;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -34,18 +52,32 @@ public class CookbookFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
+
+        if (!(context instanceof MainActivity)) {
             throw new RuntimeException(context.toString()
-                    + " must implement app.cooka.cookapp.OnFragmentInteractionListener");
+                    + " should only be attached to MainActivity.");
         }
+
+        mListener = (OnFragmentInteractionListener) context;
+
+        ((MainActivity)context).setSupportActionBar(toolbar);
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    //This method is called when a menu item from the toolbar is selected
+    private boolean onToolbarMenuItemClicked(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                //Settings action
+                Toast.makeText(this.getActivity(), R.string.action_settings, Toast.LENGTH_LONG).show();
+                return true;
+        }
+        return false;
     }
 
 }
--- app/src/main/java/app/cooka/cookapp/PlanFragment.java
+++ app/src/main/java/app/cooka/cookapp/PlanFragment.java
@@ -4,23 +4,41 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class PlanFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
+    private Toolbar toolbar;
 
     public PlanFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_plan, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //Inflate the fragment view
+        View fragmentView = inflater.inflate(R.layout.fragment_plan, container, false);
+
+        //Get the toolbar from the inflated layout and assign the menu
+        toolbar = fragmentView.findViewById(R.id.plan_toolbar);
+        toolbar.inflateMenu(R.menu.plan_toolbar);
+
+        //Setup the MenuItem click listener to handle toolbar menu actions
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return onToolbarMenuItemClicked(menuItem);
+            }
+        });
+
+        //Return the inflated view
+        return fragmentView;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -33,17 +51,31 @@ public class PlanFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
+
+        if (!(context instanceof MainActivity)) {
             throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+                    + " should only be attached to MainActivity.");
         }
+
+        mListener = (OnFragmentInteractionListener) context;
+
+        ((MainActivity)context).setSupportActionBar(toolbar);
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    //This method is called when a menu item from the toolbar is selected
+    private boolean onToolbarMenuItemClicked(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                //Settings action
+                Toast.makeText(this.getActivity(), R.string.action_settings, Toast.LENGTH_LONG).show();
+                return true;
+        }
+        return false;
     }
 }
