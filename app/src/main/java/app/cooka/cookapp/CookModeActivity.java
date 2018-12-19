9987810c0e1b44e4d804706e73ecf0b541c09cd3
Karl Möller <karl.moeller@stud.hs-hannover.de>
2018-12-19 19:59:30 +0100

    added details BottomSheet to CookModeActivity for displaying recipe overview

--- app/src/main/java/app/cooka/cookapp/CookModeActivity.java
+++ app/src/main/java/app/cooka/cookapp/CookModeActivity.java
@@ -2,12 +2,21 @@ package app.cooka.cookapp;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.button.MaterialButton;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.TransitionManager;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
 import android.widget.Toast;
 
 import com.rd.PageIndicatorView;
@@ -19,14 +28,24 @@ import com.rd.utils.DensityUtils;
  */
 public class CookModeActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = "Cook Mode";
+
     private View activityContentView;
-    private View activityControlsView;
 
     private ViewPager cardViewPager;
     private CookModeCardAdapter cardAdapter;
     private CookModeCardTransformer cardTransformer;
 
     private PageIndicatorView pageIndicatorView;
+
+    private MaterialButton detailsFab;
+    private int detailsFabState = BottomSheetBehavior.STATE_COLLAPSED;
+    private View detailsSheet;
+    private BottomSheetBehavior detailsSheetBehavior;
+    private View detailsSheetContentContainer;
+
+    private static final int DETAILS_COLLAPSED_MARGIN = DensityUtils.dpToPx(32);
+    private static final int DETAILS_EXPANDED_MARGIN = DensityUtils.dpToPx(0);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +54,6 @@ public class CookModeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cook_mode);
 
         //Main controls
-        activityControlsView = findViewById(R.id.fullscreen_content_controls);
         activityContentView = findViewById(R.id.fullscreen_content);
 
         //Card View Pager
@@ -70,6 +88,33 @@ public class CookModeActivity extends AppCompatActivity {
         //Page Indicator
         pageIndicatorView = findViewById(R.id.pageIndicatorView);
         pageIndicatorView.setViewPager(cardViewPager);
+
+        //Details Sheet
+        detailsFab = findViewById(R.id.fab);
+        detailsSheet = findViewById(R.id.details_sheet);
+        detailsSheetBehavior = BottomSheetBehavior.from(detailsSheet);
+        detailsSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        detailsSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View view, int state) {
+                if(state == BottomSheetBehavior.STATE_COLLAPSED) {
+                    setMargin(DETAILS_COLLAPSED_MARGIN);
+                    setDetailsFabState(BottomSheetBehavior.STATE_COLLAPSED);
+                }
+                else if(state == BottomSheetBehavior.STATE_EXPANDED) {
+                    setMargin(DETAILS_EXPANDED_MARGIN);
+                    setDetailsFabState(BottomSheetBehavior.STATE_EXPANDED);
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View view, float v) {
+                int margin = (int)((DETAILS_EXPANDED_MARGIN * v) + (DETAILS_COLLAPSED_MARGIN * (1-v)));
+                setMargin(margin);
+            }
+        });
+
+        detailsSheetContentContainer = findViewById(R.id.details_sheet_content);
 
         //Hide the default system ui for this activity
         hideSystemUI();
@@ -110,12 +155,58 @@ public class CookModeActivity extends AppCompatActivity {
     }
 
     public void fab_onClick(View view) {
-        Toast.makeText(this, "Hello there mister fab.", Toast.LENGTH_SHORT);
+        //Toggle the details sheet state
+        int newState = (detailsSheetBehavior.getState() == BottomSheetBehavior.STATE_EXPANDED ?
+                BottomSheetBehavior.STATE_COLLAPSED :
+                BottomSheetBehavior.STATE_EXPANDED);
+        detailsSheetBehavior.setState(newState);
+        setDetailsFabState(newState);
     }
 
     //TODO: Move method to utility class
     //NOTE: Not needed because a utility method is already included in the PageIndicatorView lib
     public static int convertDip2Pixels(Context context, int dip) {
         return (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dip, context.getResources().getDisplayMetrics());
+    }
+
+    private void animateMargin(final int newMargin) {
+
+        ConstraintLayout.LayoutParams oldParams =
+                (ConstraintLayout.LayoutParams)detailsSheetContentContainer.getLayoutParams();
+
+        final int oldMargin = oldParams.topMargin;
+
+        Animation anim = new Animation() {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                ConstraintLayout.LayoutParams params =
+                        (ConstraintLayout.LayoutParams)detailsSheetContentContainer.getLayoutParams();
+
+                params.topMargin = (int)((oldMargin * (1-interpolatedTime)) + (newMargin * interpolatedTime));
+                detailsSheetContentContainer.setLayoutParams(params);
+            }
+        };
+        anim.setDuration(500);
+        detailsSheetContentContainer.startAnimation(anim);
+    }
+
+    private void setMargin(final int newMargin) {
+        ConstraintLayout.LayoutParams params =
+                (ConstraintLayout.LayoutParams)detailsSheetContentContainer.getLayoutParams();
+
+        params.topMargin = newMargin;
+        detailsSheetContentContainer.setLayoutParams(params);
+    }
+
+    private void setDetailsFabState(int state) {
+        if(state == detailsFabState) return;
+        detailsFabState = state;
+
+        //TransitionManager.beginDelayedTransition((ViewGroup)detailsSheet);
+        int newText = (
+                state == BottomSheetBehavior.STATE_EXPANDED ?
+                        R.string.steps_fab_text :
+                        R.string.overview_fab_text);
+        detailsFab.setText(getString(newText));
     }
 }
