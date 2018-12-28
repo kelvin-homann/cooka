c5af4ef8efa4e4a9cbec3e0223b5584a209b6bda
Kelvin Homann <kelvin.homann@gmail.com>
2018-12-28 14:06:51 +0100

    created RegisterActivity and Fragment;

---
+++ app/src/main/java/app/cooka/cookapp/RegisterActivity.java
@@ -0,0 +1,14 @@
+package app.cooka.cookapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class RegisterActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+}
